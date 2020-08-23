eb8d39c6b997e92eae671223e8d1257f9e7773b1
todd <toddwunz@gmail.com>
2020-08-24 01:10:38 +1200

    update test script

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+from run import *
+
+class TestNumbers(unittest.TestCase):
+    def test_integers(self):
+        self.assertTrue(type(sumOfNumbers(1,2)) is int)
+    
+    def test_sum(self):
+        self.assertEqual(sumOfNumbers(1,2), 3)
